8dd621490994a43892be3530b84f5c16289fb7dd
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-07 23:25:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp109
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MYREGION
+            Console.WriteLine("Enter The Number: ");
+            Double Input = Convert.ToDouble(Console.ReadLine());
+            if (Input % 2 == 0)
+            {
+                Console.WriteLine("EVen NUMBER");
+
+            }
+            else
+            {
+                Console.WriteLine("Odd NUMBER");
+            }
+            #endregion
+         
+        }
+    }
+}
